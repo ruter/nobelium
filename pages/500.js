21e0273e5367a21b45bd9d8398b49022beb0e681
Ruter Lyu <ruter.lv@gmail.com>
2023-03-21 16:38:28 +0800

    Update 404 & 500 pages

--- pages/500.js
+++ pages/500.js
@@ -1,9 +1,10 @@
 import Container from '@/components/Container'
 
-export default function Custom500 () {
+export default function Page500 () {
   return (
     <Container>
-      <p style={{ textAlign: 'center', fontSize: '3em' }}>500 Internal Server Error</p>
+      <h1 className="text-5xl text-black dark:text-white text-center">500</h1>
+      <p className="text-xl text-gray-600 dark:text-gray-300 text-center">Internal Server Error</p>
     </Container>
   )
 }
